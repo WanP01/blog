9d1c2e3772f0babcb73d5eb2a60fdcb755d0076f
WanP01 <wanpwc@163.com>
2022-10-15 10:53:12 +0800

    修正

--- build/lib/blog/custom_admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.admin import AdminSite
-
-#定制site
-class CustomAdmin(AdminSite):
-        site_header = 'blog'
-        site_title =  'blog管理后台'
-        index_title = '首页'
-
-cust_site = CustomAdmin(name='cust_admin')
--- build/lib/mainblog/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Post
-
-class PostSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model =Post
-        fields = ['title','category','desc','content_html','created_time']
-
